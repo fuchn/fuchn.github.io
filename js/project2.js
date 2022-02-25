d73b78d79eba3d516dfbaea4aeec87a09161be10
fuchn <93820168+fuchn@users.noreply.github.com>
2022-02-25 18:55:07 +0800

    Initial commit

--- js/project2.js
+++ js/project2.js
@@ -5,60 +5,8 @@ new Vue({
             temp:false,
             keyWord:"",
             classname:true,
-            objs:{
-                PostID:1,
-                PostTitle:"123",
-                PostTotal:2,
-                Upvote:0,
-                PostOwner:"li",
-                Date:'2022-02-11 11:45:43',
-                PostNodes:[
-                {
-                    UserID:1,
-                    PostContent:"123",
-                    PostFloor:0,
-                    PostUpvote:1,
-                    PostDate:'2022-02-11 11:45:43',
-                    Comments:[
-                        {
-                            UserID:1,
-                            CommentContent:"123",
-                            CommentFloor:0,
-                            CommentUpvote:1,
-                            CommentDate:'2022-02-11 11:45:43',
-                        },
-                        {
-                            UserID:2,
-                            CommentContent:"456",
-                            CommentFloor:0,
-                            CommentUpvote:1,
-                            CommentDate:"2022-01-02 11:01:23"
-                        }
-                    ]
-                },
-                {
-                    UserID:2,
-                    PostContent:"456",
-                    PostFloor:0,
-                    PostUpvote:0,
-                    PostDate:'2022-02-12 11:45:48',
-                    Comments:[
-                        {
-                            UserID:1,
-                            CommentContent:"123",
-                            CommentDate:"2022-01-01 11:01:23",
-
-                        },
-                        {
-                            UserID:2,
-                            CommentContent:"456",
-                            CommentDate:"2022-01-02 11:01:23"
-                        }
-                    ]
-                }
-            ]
-        }
-    }                
+            objs:{},
+        }                
     },
     methods: {
         order(m){
