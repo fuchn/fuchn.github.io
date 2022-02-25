6d5ac0db491fbfdb7419dfefd7c8917eed5b0be2
fuchn <93820168+fuchn@users.noreply.github.com>
2022-02-25 19:22:47 +0800

    Initial commit

--- js/project2.js
+++ js/project2.js
@@ -5,7 +5,32 @@ new Vue({
             temp:false,
             keyWord:"",
             classname:true,
-            objs:{},
+            objs:{
+                PostID:1,
+                PostTitle:"123",
+                PostTotal:2,
+                Upvote:0,
+                PostOwner:"li",
+                Date:'2022-02-11 11:45:43',
+                PostNodes:[
+                {
+                    UserID:1,
+                    PostContent:"123",
+                    PostFloor:0,
+                    PostUpvote:1,
+                    PostDate:'2022-02-11 11:45:43',
+                    Comments:[
+                        {
+                            UserID:1,
+                            CommentContent:"123",
+                            CommentFloor:0,
+                            CommentUpvote:1,
+                            CommentDate:'2022-02-11 11:45:43',
+                        },
+                    ]
+                },
+            ]
+            },
         }                
     },
     methods: {
